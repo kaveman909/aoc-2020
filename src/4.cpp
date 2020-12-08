58c285c177649f753580a745f1162d2d1eff844d
David Tracy <david.tracy3@gmail.com>
2020-12-08 12:17:08 -0700

    day 4 pt 1

--- src/4.cpp
+++ src/4.cpp
@@ -14,7 +14,39 @@ using namespace fmt;
 int main(int argc, char *argv[]) {
   assert(argc == 2);
 
-  auto in = process_input(argv[1]);
+  const auto in = process_input(argv[1]);
+
+  constexpr array keys = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+
+  bool nl = true;
+  int counter = 0;
+  int found = 0;
+  for (const auto &line : in) {
+    if (line.empty()) {
+      nl = true;
+      continue;
+    }
+    if (nl) {
+      nl = false;
+      if (counter == keys.size()) {
+        // found them all!
+        found++;
+      }
+      counter = 0;
+    }
+    for (const auto &key : keys) {
+      if (line.find(key) != string::npos) {
+        counter++;
+      }
+    }
+  }
+
+  // could be one more after the loop
+  if (counter == keys.size()) {
+    found++;
+  }
+
+  print("Part 1: {}\n", found);
 
   return 0;
 }
