e56a9c561c9159d95607d8e7ad5d4eaf670c03f3
David Tracy <david.tracy3@gmail.com>
2020-12-08 08:36:23 -0700

    updating cmake to use foreach loop to simplify adding tests; added day 4 input

---
+++ src/4.cpp
@@ -0,0 +1,20 @@
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+
+#include <cassert>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#include "aoc_util.h"
+
+using namespace std;
+using namespace fmt;
+
+int main(int argc, char *argv[]) {
+  assert(argc == 2);
+
+  auto in = process_input(argv[1]);
+
+  return 0;
+}
