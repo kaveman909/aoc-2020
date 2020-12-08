98c32442ee7dafc80e63d8734ac9283e9ec3e925
David Tracy <david.tracy3@gmail.com>
2020-12-07 22:23:26 -0700

    day 2 pt 1

--- src/2.cpp
+++ src/2.cpp
@@ -1,10 +1,8 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
-#include <algorithm>
 #include <cassert>
-#include <fstream>
-#include <iostream>
+#include <regex>
 #include <string>
 #include <vector>
 
@@ -12,12 +10,35 @@
 
 using namespace std;
 using namespace fmt;
+using namespace std::string_literals;
 
 int main(int argc, char *argv[]) {
   assert(argc == 2);
 
   const auto in = process_input(argv[1]);
-  print("{}\n", in);
+  const regex parse(R"((\d+)-(\d+) (\w): (\w+))");
+  int password_counter = 0;
+
+  for (const auto &line : in) {
+    smatch m;
+    regex_search(line, m, parse);
+    const int lowest = stoi(m.str(1));
+    const int highest = stoi(m.str(2));
+    const char letter = m.str(3).c_str()[0];
+    const string password = m.str(4);
+
+    int letter_counter = 0;
+    for (const auto &c : password) {
+      if (c == letter) {
+        letter_counter++;
+      }
+    }
+    if ((letter_counter <= highest) && (letter_counter >= lowest)) {
+      password_counter++;
+    }
+  }
+
+  print("Part 1: {}\n", password_counter);
 
   return 0;
 }
