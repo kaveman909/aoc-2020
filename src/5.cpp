5c3e47a0b3596f5f37725252a5da58cc7ca7eff0
David Tracy <david.tracy3@gmail.com>
2020-12-09 23:25:38 -0700

    day 5, pt 2

--- src/5.cpp
+++ src/5.cpp
@@ -11,12 +11,16 @@
 
 #include "aoc_util.h"
 
+using namespace std;
+using namespace fmt;
+
 int main(int argc, char *argv[]) {
   assert(argc == 2);
 
   auto in = process_input(argv[1]);
 
   int max = 0;
+  vector<int> seats;
 
   for (auto &line : in) {
     for (auto &c : line) {
@@ -34,12 +38,19 @@ int main(int argc, char *argv[]) {
     const auto row = stoi(line.substr(0, 7), 0, 2);
     const auto column = stoi(line.substr(7), 0, 2);
     const auto seat_id = row * 8 + column;
+    seats.emplace_back(seat_id);
     if (seat_id > max) {
       max = seat_id;
     }
   }
-
-  fmt::print("Part 1: {}\n", max);
+  print("Part 1: {}\n", max);
+  ranges::sort(seats);
+  for (int i = 1; i < seats.size(); i++) {
+    if ((seats[i] - seats[i - 1]) == 2) {
+      print("Part 2: {}\n", seats[i] - 1);
+      break;
+    }
+  }
 
   return 0;
 }
