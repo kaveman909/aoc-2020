048436c26b8d4f305b6a94aa39ab7e1d4f28a078
David Tracy <david.tracy3@gmail.com>
2020-12-10 18:26:01 -0700

    day 6, pt. 2

--- src/6.cpp
+++ src/6.cpp
@@ -21,19 +21,44 @@ int main(int argc, char *argv[]) {
 
   string all_answers;
   vector<int> totals;
+  vector<int> totals2;
+  vector<string> group;
   for (const auto &line : in) {
     if (line.empty()) {
+      // Part 1 code
       ranges::sort(all_answers);
       auto [f, l] = ranges::unique(all_answers);
       all_answers.erase(f, l);
       totals.emplace_back(all_answers.size());
       all_answers = "";
+
+      // Part 2 code
+      map<char, int> m;
+      int counter = 0;
+      for (const auto &member : group) {
+        for (const auto &answer : member) {
+          if (m.contains(answer)) {
+            m[answer]++;
+          } else {
+            m[answer] = 1;
+          }
+        }
+      }
+      for (const auto &[key, value] : m) {
+        if (value == group.size()) {
+          counter++;
+        }
+      }
+      totals2.emplace_back(counter);
+      group.clear();
     } else {
       all_answers += line;
+      group.emplace_back(line);
     }
   }
 
   print("Part 1: {}\n", accumulate(totals.begin(), totals.end(), 0));
+  print("Part 2: {}\n", accumulate(totals2.begin(), totals2.end(), 0));
 
   return 0;
 }
