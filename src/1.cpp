f2c7f6b4f3a110a1d3c2d727b64855847bbb1f4a
David Tracy <david.tracy3@gmail.com>
2020-12-19 11:41:08 -0700

    added Rad's unordered_map solution; impressive!

--- src/1.cpp
+++ src/1.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -20,6 +21,38 @@ int main(int argc, char *argv[]) {
 
   auto v = process_input(argv[1], [](const string &x) { return stoi(x); });
 
+  //////
+  int ans = 0;
+  using namespace std::chrono;
+  auto start = high_resolution_clock::now();
+  unordered_map<int, int> m;
+
+  for (int &num : v) {
+    if (m.find(num) == m.end()) {
+      m[2020 - num] = num;
+    } else {
+      ans = num * m[num];
+    }
+  }
+
+  auto stop = high_resolution_clock::now();
+  print("Rad   Solution | Time: {} | {}\n", ans,
+        duration_cast<microseconds>(stop - start).count());
+  //////
+  ans = 0;
+  start = high_resolution_clock::now();
+  for (const auto &i : v) {
+    for (const auto &j : v) {
+      if ((i + j) == 2020) {
+        ans = i * j;
+      }
+    }
+  }
+  stop = high_resolution_clock::now();
+
+  print("Naive Solution | Time: {} | {}\n", ans,
+        duration_cast<microseconds>(stop - start).count());
+
   // Since there's lots of large numbers that won't add to 2020, will be faster
   // in the long run if we sort the list first (I would think... though could be
   // wrong!)
@@ -30,12 +63,10 @@ int main(int argc, char *argv[]) {
     for (const auto &j : v) {
       for (const auto &k : v) {
         if (!found && ((j + k) == TARGET)) {
-          print(emphasis::bold | fg(color::forest_green), "Part 1: {}\n",
-                j * k);
+          print("Part 1: {}\n", j * k);
           found = true;
         } else if ((i + j + k) == TARGET) {
-          print(emphasis::bold | fg(color::forest_green), "Part 2: {}\n",
-                i * j * k);
+          print("Part 2: {}\n", i * j * k);
           return 0;
         }
       }
