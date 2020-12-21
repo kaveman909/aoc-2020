3a44b94ddb3363156a5b19fced0d2a5c97849770
David Tracy <david.tracy3@gmail.com>
2020-12-20 21:30:39 -0700

    day 11, pt 1

--- src/11.cpp
+++ src/11.cpp
@@ -15,9 +15,30 @@
 using namespace std;
 using namespace fmt;
 
-enum class Seat { FLOOR, EMPTY, TAKEN };
+enum class Seat { FLOOR, EMPTY, OCCUPIED };
 
-auto get_neighbors(const vector<vector<Seat>> seating, const int x,
+template <>
+struct fmt::formatter<Seat> : formatter<string_view> {
+  // parse is inherited from formatter<string_view>.
+  template <typename FormatContext>
+  auto format(Seat c, FormatContext &ctx) {
+    string_view name = "unknown";
+    switch (c) {
+      case Seat::FLOOR:
+        name = ".";
+        break;
+      case Seat::EMPTY:
+        name = "L";
+        break;
+      case Seat::OCCUPIED:
+        name = "#";
+        break;
+    }
+    return formatter<string_view>::format(name, ctx);
+  }
+};
+
+auto get_neighbors(const vector<vector<Seat>> &seating, const int x,
                    const int y) {
   vector<Seat> neighbors;
   const int x_min = (x == 0) ? x : x - 1;
@@ -25,9 +46,9 @@ auto get_neighbors(const vector<vector<Seat>> seating, const int x,
   const int y_min = (y == 0) ? y : y - 1;
   const int y_max = (y == (seating.size() - 1)) ? y : y + 1;
 
-  for (int xi = x_min; xi < x_max; xi++) {
-    for (int yi = y_min; yi < y_max; yi++) {
-      if ((x != xi) && (y != yi) && (seating[yi][xi] != Seat::FLOOR)) {
+  for (int xi = x_min; xi <= x_max; xi++) {
+    for (int yi = y_min; yi <= y_max; yi++) {
+      if (((x != xi) || (y != yi)) && (seating[yi][xi] != Seat::FLOOR)) {
         neighbors.emplace_back(seating[yi][xi]);
       }
     }
@@ -49,12 +70,46 @@ int main(int argc, char *argv[]) {
 
   const int x_max = seating[0].size();
   const int y_max = seating.size();
-
-  for (int x = 0; x < x_max; x++) {
+  auto seating_copy = seating;
+  auto loop_counter = 0;
+  while (1) {
+    loop_counter++;
     for (int y = 0; y < y_max; y++) {
-      const auto neighbors = get_neighbors(seating, x, y);
+      for (int x = 0; x < x_max; x++) {
+        auto &current_seat = seating_copy[y][x];
+        if (current_seat == Seat::FLOOR) {
+          continue;
+        }
+        const auto neighbors = get_neighbors(seating, x, y);
+        if (current_seat == Seat::EMPTY) {
+          // If a seat is empty (L) and there are no occupied seats adjacent to
+          // it, the seat becomes occupied
+          if (ranges::none_of(neighbors, [](Seat seat) {
+                return seat == Seat::OCCUPIED;
+              })) {
+            current_seat = Seat::OCCUPIED;
+          }  // no else
+        } else if (current_seat == Seat::OCCUPIED) {
+          // If a seat is occupied (#) and four or more seats adjacent to it are
+          // also occupied, the seat becomes empty.
+          if (ranges::count(neighbors, Seat::OCCUPIED) >= 4) {
+            current_seat = Seat::EMPTY;
+          }  // no else
+        }    // Otherwise, the seat's state does not change.
+      }
+    }
+    if (seating != seating_copy) {
+      seating = seating_copy;
+    } else {
+      int sum = 0;
+      for (const auto &row : seating) {
+        // print("{}\n", join(row, ""));
+        sum =
+            transform_reduce(row.begin(), row.end(), sum, plus(),
+                             [](Seat seat) { return seat == Seat::OCCUPIED; });
+      }
+      print("Part 1: {}\n", sum);
+      return 0;
     }
   }
-
-  return 0;
 }
